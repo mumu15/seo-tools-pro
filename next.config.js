5fff831c1476da707d985cec4c57cc8203ecb09d
mumu15 <access2munir@gmail.com>
2026-02-28 02:54:41 +0100

    Add redirect from non-www to www

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'wordcountertool.net' }],
+        destination: 'https://www.wordcountertool.net/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
+
 module.exports = nextConfig
